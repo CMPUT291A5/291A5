2082e45d7e5022175339f1a121818364697714db
xinyuan-zhao <55670831+xinyuan-zhao@users.noreply.github.com>
2021-04-11 18:49:00 +0800

    Add files via upload

---
+++ a5t1listingstest.sql
@@ -0,0 +1,2 @@
+SELECT * FROM listings ORDER BY id LIMIT 3;
+SELECT MIN(host_id), MAX(host_id), AVG(host_id), COUNT(host_id) FROM listings;
---
+++ a5t1reviewstest.sql
@@ -0,0 +1,2 @@
+SELECT MIN(id), MAX(id), AVG(id), COUNT(id) FROM reviews; 
+SELECT * from reviews order by listing_id LIMIT 3 ;
