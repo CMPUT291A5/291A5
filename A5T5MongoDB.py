b9012cef078b0fe7cbaec3cf22a9fad878623a1e
xinyuan-zhao <55670831+xinyuan-zhao@users.noreply.github.com>
2021-04-13 04:51:53 +0800

    Add files via upload

---
+++ A5T5MongoDB.py
@@ -0,0 +1,24 @@
+
+import pymongo
+
+
+def main():
+    print("Task 5 MongoDB")
+    # open db
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    mydb = myclient["A5db"]
+    mycoll = mydb["listings"]
+    neighbourhood = input("Please enter neighbourhood:  ")
+    run_time = input("Please enter run-time: ")
+
+    for x in mycoll.aggregate([
+            {
+                '$match': {'neighbourhood': neighbourhood}
+            },
+            {
+                '$group': {'_id': 'neighbourhood', 'rental_cost_avg': {'$avg': '$price'}}
+            }
+        ]):
+        print(x)
+
+main()
