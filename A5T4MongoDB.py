a79635126884b2c84c3bb4e96869c873b90839d3
xinyuan-zhao <55670831+xinyuan-zhao@users.noreply.github.com>
2021-04-13 17:39:35 +0800

    Update A5T4MongoDB.py

--- A5T4MongoDB.py
+++ A5T4MongoDB.py
@@ -1,15 +1,19 @@
-# 291 A5T4MongoDB
+# 291 A5 Task4 MongoDB
 
 import pymongo
-
+import time
 
 def main():
-    print("Task 4")
+    print("Task 4 MongoDB")
     # open db
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
     mydb = myclient["A5db"]
     mycoll = mydb["reviews"]
+    start_time = time.time()
     for x in mycoll.find({"comments":  ""},{"_id": 0, "comments": 0}).limit(10).sort([('Listing_id', 1)]):
-        print(x)
 
+        print(x)
+    end_time = time.time()
+    run_time = (end_time - start_time) * 1000
+    print("Run time for Task 4 SQLite is:{}ms.".format(run_time))
 main()
