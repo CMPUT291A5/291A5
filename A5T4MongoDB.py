98ad9e65273176b9d52edf7b2beb3ddff8356fc2
xinyuan-zhao <55670831+xinyuan-zhao@users.noreply.github.com>
2021-04-14 04:13:36 +0800

    Update A5T4MongoDB.py

--- A5T4MongoDB.py
+++ A5T4MongoDB.py
@@ -2,15 +2,21 @@
 import pymongo
 
 
+# {'$match': {'$or':{'comments':{'$eq':''},{ '$exists': False } }}},
+
 def main():
     print("Task 4 MongoDB")
     # open db
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
     mydb = myclient["A5db"]
-    for x in mydb.reviews.aggregate([
-            {'$lookup': {'from':"listings", 'localField':"listing_id", 'foreignField': "id", 'as': "listings_docs" }},
-            {'$match': {'comments':{'$ne':''}}},{'$sort': {'id':1}},
-            {'$limit': 10}, {'$project':{'_id':0,'id':1}}]):
+    for x in mydb.listings.aggregate([
+        {'$lookup': {'from': "reviews", 'localField': "id", 'foreignField': "listing_id", 'as': "reviews_list"}},
+        {'$match': {"reviews_list": []}},
+        {'$project': {'_id': 0, 'id': 1}},
+        {'$sort': {'id': 1}},
+        {'$limit': 10}]):
         print(x)
 
+
 main()
+
