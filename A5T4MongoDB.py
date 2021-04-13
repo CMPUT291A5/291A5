46983ef79fa99c56c4d09099f1211f565ec9be71
xinyuan-zhao <55670831+xinyuan-zhao@users.noreply.github.com>
2021-04-14 03:59:02 +0800

    Update A5T4MongoDB.py

--- A5T4MongoDB.py
+++ A5T4MongoDB.py
@@ -1,19 +1,16 @@
 # 291 A5 Task4 MongoDB
-
 import pymongo
-import time
+
 
 def main():
     print("Task 4 MongoDB")
     # open db
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
     mydb = myclient["A5db"]
-    mycoll = mydb["reviews"]
-    start_time = time.time()
-    for x in mycoll.find({"comments":  ""},{"_id": 0, "comments": 0}).limit(10).sort([('Listing_id', 1)]):
-
+    for x in mydb.reviews.aggregate([
+            {'$lookup': {'from':"listings", 'localField':"listing_id", 'foreignField': "id", 'as': "listings_docs" }},
+            {'$match': {'comments':{'$ne':''}}},{'$sort': {'id':1}},
+            {'$limit': 10}, {'$project':{'_id':0,'id':1}}]):
         print(x)
-    end_time = time.time()
-    run_time = (end_time - start_time) * 1000
-    print("Run time for Task 4 MongoDB is:{}ms.".format(run_time))
+
 main()
