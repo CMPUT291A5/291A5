0a991a13113d4ecbf024e812b979d05ae8eeaba3
xinyuan-zhao <55670831+xinyuan-zhao@users.noreply.github.com>
2021-04-13 17:47:29 +0800

    Update A5T5SQLite.py

--- A5T5SQLite.py
+++ A5T5SQLite.py
@@ -1,24 +1,26 @@
-# 291 A5 Task5
+# 291 A5 Task5 SQLite
 import sqlite3
-
+import time
 
 def task5_sql(cursor):
-    print("Task 5")
+    print("Task 5 SQLite")
     neighbourhood = input("Please enter neighbourhood:  ")
-    run_time = input("Please enter run-time: ")
-
+    start_time = time.time()
     try:
         sql = ''' SELECT avg(price) as average_rental
         FROM listings
         WHERE neighbourhood = :Neighbourhood
         '''
         cursor.execute(sql, {'Neighbourhood': neighbourhood})
+        end_time = time.time()
+        run_time = (end_time - start_time)* 1000
         result = cursor.fetchall()
         count = len(result)
         # check if there is data
         if count > 0:
             for i in result:
                 print(int(i[0]))
+            print("Run time for Task 5 SQLite is:{}ms.".format(run_time))
         else:
             print("None of them fit.\n")
 
